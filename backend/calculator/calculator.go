291e0a147627eb5e6fab3dc0433e97e5a1696010
sogvazulchik <sogvazulchik@gmail.com>
2025-09-07 13:58:55 +0300

    first version with 'expr' library

--- backend/calculator/calculator.go
+++ backend/calculator/calculator.go
@@ -1,8 +1,38 @@
-package calculator
+package main
+
+import (
+	"github.com/expr-lang/expr"
+)
 
 // Getting expression from frontend -> evaluating
 // its value or giving exception if there's error
 func Evaluate(expression string) (float64, error) {
-	// заглушка
-	return 0, nil
+	program, err := expr.Compile(expression, expr.AsFloat64())
+	if err != nil {
+		return 0, err
+	}
+
+	output, err := expr.Run(program, nil)
+	if err != nil {
+		return 0, err
+	}
+
+	result, ok := output.(float64)
+	if !ok {
+		return 0, err
+	}
+
+	return result, nil
 }
+
+// usage:
+/*
+ * res, err := calculator.Evaluate("1/4 + 2 * (3 - 14)")
+ *
+ * if err != nil {
+ *   // couldnt evaluate
+ * }
+ *
+ * // else result in 'res'
+ *
+ */
