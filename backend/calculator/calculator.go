db78692d7cb1d423b4f658cc69ebb5219e8663b8
sogvazulchik <sogvazulchik@gmail.com>
2025-09-07 13:35:50 +0300

    заглушка for calculator.Evaluate(string) -> (float64, error)

---
+++ backend/calculator/calculator.go
@@ -0,0 +1,8 @@
+package calculator
+
+// Getting expression from frontend -> evaluating
+// its value or giving exception if there's error
+func Evaluate(expression string) (float64, error) {
+	// заглушка
+	return 0, nil
+}
